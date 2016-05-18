95ccbea3261143827119d4a1c913f3a9e4be0da8
omnoya <omnoya@gmail.com>
2016-05-18 14:54:14 +0200

    réparation

--- src/ReceptionBundle/Controller/DefaultController.php
+++ src/ReceptionBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ReceptionBundle:Default:index.html.twig');
+        return $this->render('ReceptionBundle:soiree:index');
     }
 }
