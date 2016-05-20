658a6497f1f984988bc074147bb9e533310d1965
omnoya <omnoya@gmail.com>
2016-05-20 14:31:52 +0200

    avant presentation

--- src/ReceptionBundle/Controller/DefaultController.php
+++ src/ReceptionBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ReceptionBundle:soiree:index');
+        return $this->render(':default:index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
+        ));
     }
 }
